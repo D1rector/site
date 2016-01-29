f5d78d09a16742cb1250ffd14b982fdb06f70a4a
D1rector <edgar_kurkchi@mail.ru>
2016-01-29 14:57:18 +0400

    second commit

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -70,7 +70,54 @@ alert ('Ошибка..');
 	<div id="content" class="site-content">
             
 
-
+<!-- Ссылки на вызов модальных окон-->
+ <a href="#win1" class="button button-green">Open Window1</a>
+ <a href="#win2" class="button button-red">Открыть окно 2</a>
+ <a href="#win3" class="button button-blue">Видео в окне 3</a>
+ <a href="#win4" class="button button-orange">Фото в окне 4</a>
+<!-- Вызов окна через миниатюру изображения -->
+<a href="#win5"><img title="" src="img/realism_lrg.jpg" width="150" height="150" alt="" /></a>
+<!-- Модальное окно №1 -->
+   <a href="#x" class="overlay" id="win1"></a>
+   <div class="popup">
+    <form> 
+	<fieldset>
+<legend>Leave your request for event</legend>
+Your name <br>
+<input type="text" name="Yourname" placeholder="Give me your soul">
+<input type="text" name="event" placeholder="The name of the event?">
+<input type="date" name="time" placeholder="What time?">
+<input type="text" name="place" placeholder="Where?" height="200px">
+<input type="submit" name="verify" value="Send">
+</fieldset>
+</form>
+    <a class="close"title="Закрыть" href="#close"></a>
+    </div>
+<!-- Модальное окно №2 -->
+    <a href="#x" class="overlay" id="win2"></a>
+    <div class="popup">
+     Здесь вы можете разместить любое содержание, текст с картинками или видео! 
+    <a class="close" title="Закрыть" href="#close"></a>
+    </div>
+<!-- Модальное окно №3 -->
+        <a href="#x" class="overlay" id="win3"></a>
+        <div class="popup">
+<h2>Заголовок</h2>
+      Здесь вставляете видео своё или с любого стороннего ресурса, YouTube, Vimeo и т.д.(iframe, embed)...
+        <a class="close" title="Закрыть" href="page.html" onclick="return false"></a><!-- Выключение видео при закрытии окна -->
+        </div>
+<!-- Модальное окно №4 -->
+<a href="#x" class="overlay" id="win4"></a>
+        <div class="popup">
+<img class="is-image" src="ваша-картинка.jpg" alt="" />
+        <a class="close" title="Закрыть" href="#close"></a>
+        </div>
+<!-- Модальное окно №5 -->
+        <a href="#x" class="overlay" id="win5"></a>
+        <div class="popup">
+<img class="is-image" src="ваша-картинка.jpg" alt="" />
+<a class="close" title="Закрыть" href="#close"></a>
+        </div>
 
  
 
